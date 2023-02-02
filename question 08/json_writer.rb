7fd7df7a16ecb9ff317bd06c2d37b3c74e650504
Henrich Jones <henrichjones@gmail.com>
2023-02-02 09:55:54 -0300

    create class for jsonwriter

---
+++ question 08/json_writer.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class JsonWriter
+  class << self
+    def write!
+      File.write('file.json', content)
+      puts 'Json file created with success!'
+    end
+
+    private
+
+    def content
+      '{
+        "breakfast_menu": {
+          "food": [
+            {
+              "name": "Belgian Waffles",
+              "price": "$5.95",
+              "description": "Two of our famous Belgian Waffles with plenty of real maple syrup",
+              "calories": 650
+            },
+            {
+              "name": "Strawberry Belgian Waffles",
+              "price": "$7.95",
+              "description": "Light Belgian waffles covered with strawberries and whipped cream",
+              "calories": 900
+            },
+            {
+              "name": "Berry-Berry Belgian Waffles",
+              "price": "$8.95",
+              "description": "Belgian waffles covered with assorted fresh berries and whipped cream",
+              "calories": 900
+            }
+          ]
+        }
+      }'
+    end
+  end
+end
+
